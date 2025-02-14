2e7799a58628a65bc78bc1cd08a91057cb01fcf4
BTHung2747 <165037475+BTHung2747@users.noreply.github.com>
2025-02-14 13:30:24 +0700

    Update index.tsx

--- DAY-2/src/pages/TrangChu/index.tsx
+++ DAY-2/src/pages/TrangChu/index.tsx
@@ -2,25 +2,21 @@ import { Card } from 'antd';
 import './components/style.less';
 import { unitName } from '@/services/base/constant';
 import { useModel } from 'umi';
-import TodoList from '../Todolist/TodoList';
 
 const TrangChu = () => {
-  const { data } = useModel('randomuser');
+	const { data } = useModel('randomuser');
 
-  return (
-    <Card bodyStyle={{ height: '100%' }}>
-      <div>
-        <TodoList />
-      </div>
-      <div className='home-welcome'>
-        <div>
-          <b>{data.length} users</b>
-        </div>
-        <h1 className='title'>THỰC HÀNH LẬP TRÌNH WEB</h1>
-        <h2 className='sub-title'>{unitName.toUpperCase()}</h2>
-      </div>
-    </Card>
-  );
+	return (
+		<Card bodyStyle={{ height: '100%' }}>
+			<div className='home-welcome'>
+				<div>
+					<b>{data.length} users</b>
+				</div>
+				<h1 className='title'>THỰC HÀNH LẬP TRÌNH WEB</h1>
+				<h2 className='sub-title'>{unitName.toUpperCase()}</h2>
+			</div>
+		</Card>
+	);
 };
 
 export default TrangChu;
